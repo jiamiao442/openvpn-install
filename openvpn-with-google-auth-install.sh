fb8210826eb1a07de449e00fb7af30f4d92e7786
Administrator <admin@example.com>
2024-11-15 15:31:01 +0800

    update dns

--- openvpn-with-google-auth-install.sh
+++ openvpn-with-google-auth-install.sh
@@ -344,7 +344,7 @@ server 10.8.0.0 255.255.255.0" > /etc/openvpn/server/server.conf
 			echo 'push "dhcp-option DNS 94.140.15.15"' >> /etc/openvpn/server/server.conf
 		;;
 		7)
-			echo 'push "#dhcp-option DNS 94.140.14.14"' >> /etc/openvpn/server/server.conf
+			echo '#push "dhcp-option DNS 94.140.14.14"' >> /etc/openvpn/server/server.conf
 		;;
 	esac
 	# echo 'push "block-outside-dns"' >> /etc/openvpn/server/server.conf
